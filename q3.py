a2315a2bed58263f1a0f2ec73db7fc4f09be0979
Vinay Singh Rathore <133983041+Vin-24@users.noreply.github.com>
2023-07-02 20:32:32 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,5 @@
+# write the program to find the area of the triangle
+height=float(input("enter the height"))
+base=float(input("enter the base"))
+area = (0.5)*base*height
+print("the area of the triangle is:", area)
