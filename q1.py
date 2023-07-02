0192ff24786c3a084a674fdb9ae16596fd9c9f2a
Vinay Singh Rathore <133983041+Vin-24@users.noreply.github.com>
2023-07-02 20:31:20 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,12 @@
+## write a program to add two number
+# n1=38
+# n2=44
+
+# print("the sum is: ", n1+n2)
+
+#or
+
+n1= float(input("enter the number1"))
+n2= float(input("enter the number1"))
+
+print("the sum is:", n1+n2)
