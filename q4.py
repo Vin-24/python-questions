b7420840fcd58a4aeac21dc87acf4022a05f0a22
Vinay Singh Rathore <133983041+Vin-24@users.noreply.github.com>
2023-07-04 15:19:56 +0530

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,21 @@
+#w.a.p to swap two variables
+# x=14
+# y=23
+
+# z=x
+
+# print("the value of z is:",z)
+# x=y
+# print("the value of x is", x)
+# y=z
+# print("the value of y is", y)
+
+        #or
+
+x=12
+y=13
+
+x, y = y, x
+print("the value of x is:", x)
+print("the value of y is:", y)
+
