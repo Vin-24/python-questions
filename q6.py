1f1a1e7af4b503f2f16cec9eb35842a3437b9ef1
Vinay Singh Rathore <133983041+Vin-24@users.noreply.github.com>
2023-07-04 15:22:39 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,8 @@
+#w.a.p to check if the given number is positive  negative or zero
+n=int(input("enter the number"))
+if n==0:
+    print("the number is zero")
+elif n<0:
+    print("the number is negative")
+else:
+    print("the number is positive")    
